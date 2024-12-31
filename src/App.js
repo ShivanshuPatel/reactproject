bd974cc49667c47a27671bf695f31ca7f0babf21
Shivanshu Patel <119284473+ShivanshuPatel@users.noreply.github.com>
2024-12-31 14:18:56 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div className='bg-orange-400'>SHIVANSHU PATEL</div>
+        <div>HELLO</div>
+    </div>
+  );
+}
+
+export default App;
