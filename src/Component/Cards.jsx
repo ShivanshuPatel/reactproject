dd73759acdebdf3404c3f622399105cdc68825ba
shyam-patil <shyamchoudharyrs@gmail.com>
2025-01-13 14:43:15 +0530

    Update Cards.jsx

--- src/Component/Cards.jsx
+++ src/Component/Cards.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 function Cards () {
   return (
-    <div className='text-red-950 text-4xl font-extrabold'>INDIA</div>
+    <div className='text-red-950 text-4xl font-extrabold'>INDIA INDIA</div>
   )
 }
 
